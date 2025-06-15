ec702cf98d77bbee0da49fc1ba65cfef8fb1f1d1
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-16 00:19:07 +0500

    Using comments

---
+++ Chapter 1/comments.py
@@ -0,0 +1,22 @@
+import pyjokes
+
+# This is a random joke
+# we use ctrl+/ for single line comments
+
+joke = pyjokes.get_joke()
+print (joke)
+
+"""
+Hello!
+How was your day?
+GoodBye
+"""
+
+# we use triple-double quotes """  something else  """ for more than one or multiline comments 
+
+# Hi 
+# Hello 
+# Excellent 
+# Bad   
+
+# simply select all the multilines and press ctrl+/ to make them single line comments 
