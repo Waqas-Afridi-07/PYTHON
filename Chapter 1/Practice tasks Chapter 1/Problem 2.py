5a389a065929d0b7b5351962a341162122e053a6
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-16 01:06:16 +0500

    Printing table of 5 using REPL or windows powershell

---
+++ Chapter 1/Practice tasks Chapter 1/Problem 2.py
@@ -0,0 +1,28 @@
+# Printing table of 5 using REPL or Terminal of system or Windows Powershell (not vs code)
+
+# open terminal and write python to go into Python
+
+# simply print this step by step:-
+
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> 5*1
+# 5
+# >>> 5*2
+# 10
+# >>> 5*3
+# 15
+# >>> 5*4
+# 20
+# >>> 5*5
+# 25
+# >>> 5*6
+# 30
+# >>> 5*7
+# 35
+# >>> 5*8
+# 40
+# >>> 5*9
+# 45
+# >>> 5*10
+# 50
+# >>> in windows powershell
