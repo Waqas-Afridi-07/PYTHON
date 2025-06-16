f5bdb7d1afa0c4fb7791b4db2148d78b328999ee
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-16 17:23:04 +0500

    Rules for naming variables

---
+++ Chapter 2/03-rules for naming variables.py
@@ -0,0 +1,9 @@
+a = 1 # variable name can start with an alphabet
+
+1 = a # variable name can start with a digit or number
+
+_ali # variable name can start with an underscore 
+
+ibrar = 17 # variable name can start with alphabetical words
+
+# @asim or n@sir # variable name can't start with special characters like @, !, $, & etc only _underscore at the beginning or end is allowed
