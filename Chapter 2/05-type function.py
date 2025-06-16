de7e30fe0549ca1a013857fb6cb69a3935206f3b
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-16 21:47:22 +0500

    type() function

---
+++ Chapter 2/05-type function.py
@@ -0,0 +1,21 @@
+# The function "type()"is used to see the data type.
+
+a = 31 # int data type
+t = type(a)
+print(t)
+
+a = 31.4 # float data type
+t = type(a)
+print(t)
+
+a = "Asim" # string data type
+t = type(a)
+print(t)
+
+a = True # bool data type
+t = type(a)
+print(t)
+
+a = None # None data type
+t = type(a)
+print(t)
