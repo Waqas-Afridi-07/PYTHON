e7e77480dcfb9f22406c19324f5d0545b7674a0e
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-16 17:00:35 +0500

    learning about variables

---
+++ Chapter 2/02-data types.py
@@ -0,0 +1,9 @@
+a = 3 # a is an integer data type variable
+
+b = 8.6 # b is a float data type variable
+
+c = "waqas" # c is a string data type variable
+
+d = True # d is a bool data type variable
+
+e = None # e is a none data type variable
