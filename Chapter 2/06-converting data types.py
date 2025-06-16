94f9d54fb2870bcef05b761a64c2458aaf435ce1
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-16 22:30:47 +0500

    Converting from one data type to another

--- Chapter 2/06-converting data types.py
+++ Chapter 2/06-converting data types.py
@@ -1,19 +1,19 @@
 a = 21
 b = str(a) # int to string
 t = type(b)
-print(t) 
+print(b) 
 
 a = 21.0
 b = int(a) # float to integer
 t = type(b)
-print(t)
+print(b)
 
 a = "21"
 b = int(a) # string to integer
 t = type(b)
-print(t)
+print(b)
 
 a = True
 b = float(a) # boolean  to float
 t = type(b)
-print(t)
+print(b)
