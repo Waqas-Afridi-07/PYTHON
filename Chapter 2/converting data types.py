87f117d6796983f35c213615852d62632e230be1
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-16 22:09:35 +0500

    Conversion of data types

---
+++ Chapter 2/converting data types.py
@@ -0,0 +1,19 @@
+a = 31
+b = str(31) # integer to string data type
+t = type(b)
+print(t)
+
+a = 31
+b = float(a) # integer to float
+t = type(b)
+print(t)
+
+a = "32"
+b = int(a) # string to integer
+t = type(b)
+print(t) 
+
+a = 32.0
+b = int(a) # float to integer 
+t = type(b)
+print(t)
