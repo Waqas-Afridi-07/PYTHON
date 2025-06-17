b77c06b7b0cfd7df9e59e7304f26e03124426ef6
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-17 18:46:38 +0500

    Learning about replace funtion

---
+++ Chapter 3/04-String functions.py
@@ -0,0 +1,22 @@
+name = "Waqas"
+
+print(len(name)) # for finding length of string
+print(name.endswith("qas")) # for verifying ending of string
+
+name = "waqas"
+print(name.capitalize()) # for capitalizing first character
+
+string = "abracadabra"
+count = string.count("a") # for counting characters given by the user
+print(count)
+
+s = "Hi, homies"
+print(s.find("h")) # for finding characters of the user's will
+
+t = "Thanks Enola!"
+print(t.find("a", 3))
+
+text = "I like apples"
+new_text = text.replace("apples", "mangoes")
+
+print(new_text)  # Output: I like mangoes
