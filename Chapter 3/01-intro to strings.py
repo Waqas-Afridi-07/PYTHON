b869c71ff7054eef8ccbf737e25d357e3db90a21
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-17 17:00:23 +0500

    Introduction to strings

---
+++ Chapter 3/01-intro to strings.py
@@ -0,0 +1,14 @@
+# String is a sequence of characters enclosed in quotes.
+# String can be written in single quotes (''), double quotes ("") and triple quotes (""").
+
+name = "Waqas" # -> length = 5
+# A string in python can be sliced for getting a part of string.
+# The index of a string starts from 0 to (length -1). First index is included, last is not included.
+
+nameshort = name[2:4]
+print(nameshort)
+
+# in case we need only one character we'll use the following syntax :-
+
+character = name[4]
+print(character)
