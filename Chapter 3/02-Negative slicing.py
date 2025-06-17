d6ee12b7b9f2ee84a7dbf0b9f84d2e85e0316264
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-17 17:44:44 +0500

    Learning about negative slicing

---
+++ Chapter 3/02-Negative slicing.py
@@ -0,0 +1,8 @@
+name = "Asim"
+
+print(name[-3: -1])
+print(name[-4: -1])
+
+print(name[:3]) # means 0:3
+print(name[2:]) # means 2:4 or full length of string written (last index will be included)
+
