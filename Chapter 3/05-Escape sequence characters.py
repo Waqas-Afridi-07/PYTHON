062cee693ffb2aba01e8f00893e9766abe687178
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-19 14:50:41 +0500

    Learning about escape sequence characters

---
+++ Chapter 3/05-Escape sequence characters.py
@@ -0,0 +1,17 @@
+t = "Waqas is a good boy\nbut not a bad boy" # \n for a new line
+print(t)
+
+e = "Waqas is a good boy\tbut not a bad boy" # \t for giving space equal to "Tab" button
+print(e)
+
+a = "Waqas is a good boy but not a \"bad\" boy" # \" \" used to wrap a word in double quotes into a string
+print(a)
+
+b = "C\\Users\\Waqas" # \\ \\ used for single backslash in a string
+print(b)
+
+d = 'He\'s fine' # \' used for single quote inside a string
+print(d)
+
+f = "Hello\rHi" # \r used for replacing 
+print(f)
