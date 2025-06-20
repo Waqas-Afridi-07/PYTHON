81868abb89a75658737c937393a51b7afa0962d9
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-20 21:19:19 +0500

    Learning about lists

---
+++ Chapter 4/01-lists.py
@@ -0,0 +1,11 @@
+# Arrays are called lists in pyhton
+# lists store set of values of any data type
+
+me = ["waqas", 17, "1st year", 45.6, "friends", "moeed", "hassan", False]
+
+print(me[5]) # "[]" syntax is used for list
+
+# lists are mutable (changeable) 
+
+me [5] = "Daniyal" # we can change the value in list
+print(me[5])
