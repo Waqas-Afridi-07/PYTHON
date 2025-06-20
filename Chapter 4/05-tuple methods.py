bc36636e28c36dfce045c7628b68b4caf2f02001
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-20 22:56:08 +0500

    Tuple methods

---
+++ Chapter 4/05-tuple methods.py
@@ -0,0 +1,9 @@
+# Because tuples are immutable so it has only two built-in functions ".count()" and ".index()"
+# so, there are very few tuple methods 
+
+a = ("waqas", 45.9, 1, 2)
+
+print(a.count("waqas")) # counts elements
+
+a = ("waqas", 45.9, 1, 2)
+print(a.index(2)) # finds the index of value
