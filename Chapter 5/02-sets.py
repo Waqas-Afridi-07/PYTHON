5fa661dac93ee87ca452e717a77373090743205e
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-23 15:10:44 +0500

    Learning about sets

---
+++ Chapter 5/02-sets.py
@@ -0,0 +1,15 @@
+# Sets are collection of non repetitive or unordered elements.
+
+
+d = {} # Empty dictionary.
+print(d,type(d))
+
+# s = {} returns <class 'dict'> don't use s = {} as it'll create an empty dictionary.
+
+# We can create an empty set like this :-
+
+s = set()
+print(s,type(s))
+
+S = {70,1,65,9,4,} # As it is non reprtitive it only takes 4 one time and the order is not fixed.
+print(S)
