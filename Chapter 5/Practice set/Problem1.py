e290764bcd09367c3f8dcacd23fb6869d01cffe4
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-23 16:21:35 +0500

    Printing meaning of words

---
+++ Chapter 5/Practice set/Problem1.py
@@ -0,0 +1,9 @@
+words = {
+    "Chai" : "Tea",
+   "Drakht" : "Tree",
+   "Insan" : "Human",
+   "Janwar" : "Animal",
+   "aam" : "Mango"
+}
+word = input("Enter the word you want meaning of : ")
+print(words[word]) 
