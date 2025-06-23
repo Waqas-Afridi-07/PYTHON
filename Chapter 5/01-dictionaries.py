fddcc7c4c5f2a9dcbe1ed7cb018e75a8492cb3a6
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-23 12:44:34 +0500

    Dictionary in python

---
+++ Chapter 5/01-dictionaries.py
@@ -0,0 +1,12 @@
+# it is a collection of key value pairs.
+
+marks = {
+    "Waqas" : 23,
+    "Ibrar" : 87,
+    "Nasir" : 100
+}
+
+print(marks,type(marks))
+print(marks["Waqas"]) # we can individually execute the corresponding value of key in dictionaries and it
+                        # very helpful in bigger dictionary codes 
+
