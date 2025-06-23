25c4231fa3e0d35556e59811cb45c9a97eaeff3a
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-23 19:11:42 +0500

    Printing friend's fav language

---
+++ Chapter 5/Practice set/Problem6.py
@@ -0,0 +1,24 @@
+l = {}
+print(type(l))
+
+name = input("Enter friend's name : ") 
+lang = input("Favorite language : ")
+
+l.update({name: lang})
+
+name = input("Enter friend's name : ") 
+lang = input("Favorite language : ")
+
+l.update({name: lang})
+
+name = input("Enter friend's name : ") 
+lang = input("Favorite language : ")
+
+l.update({name: lang})
+
+name = input("Enter friend's name : ") 
+lang = input("Favorite language : ")
+
+l.update({name: lang})
+
+print(l)
