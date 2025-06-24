c2669ec180a6e5a136443bb505669900c9d6b792
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-24 12:47:58 +0500

    If-Else statements

---
+++ Chapter 6/conditionals.py
@@ -0,0 +1,11 @@
+# They are multiway decision taken by our program due to certain conditions in our code.
+
+a = int(input("Enter your age : "))
+
+if(a>=18):
+    print("You are above the age of consent")
+
+else:
+    ("You are below the age of consent")
+
+print("End")    
