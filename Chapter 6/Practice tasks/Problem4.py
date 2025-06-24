d70cd5c4ef8dbe26f2b9bb86e2619d56cdc6009c
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-24 14:58:52 +0500

    username checking

---
+++ Chapter 6/Practice tasks/Problem4.py
@@ -0,0 +1,7 @@
+username = input("Enter the name : ")
+
+if(len(username)<=10):
+    print("Yes, your username contains less than 10 characters")
+
+else:
+    print("your username contains more than 10 characters")    
