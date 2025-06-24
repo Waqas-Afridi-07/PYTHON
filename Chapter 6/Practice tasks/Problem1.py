0a4bd12a935a9a36e7bd38abd606196c59202c5d
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-06-24 13:54:35 +0500

    Finding the greatest of four

---
+++ Chapter 6/Practice tasks/Problem1.py
@@ -0,0 +1,16 @@
+n1 = int(input("Enter number 1 : "))
+n2 = int(input("Enter number 2 : "))
+n3 = int(input("Enter number 3 : "))
+n4 = int(input("Enter number 4 : "))
+
+if(n1>n2 and n1>n3 and n1>n4):
+    print("Greatest number is n1 :", n1)    
+
+elif(n2>n1 and n2>n3 and n2>n4):
+    print("Greatest number is n2 :", n2)    
+
+elif(n3>n1 and n3>n2 and n1>n4):
+    print("Greatest number is n3 :", n3)    
+
+elif(n4>n1 and n4>n2 and n4>n3):
+    print("Greatest number is n4 :", n4)    
