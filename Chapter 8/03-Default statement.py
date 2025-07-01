2a4ae174ed81a1c5e3b89d164fe4be92e8b0daff
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-01 18:14:39 +0500

    Default argument in function

---
+++ Chapter 8/03-Default statement.py
@@ -0,0 +1,7 @@
+def goodDay(name, ending= "Thank You"): # ending= "Thank You" (Defult Statement).
+    print(f"Good day, " + name)
+    print(ending)
+
+goodDay("Waqas", "Thanks") 
+goodDay("Nasir")
+
