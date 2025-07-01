06ff7c7e773d48841e3eb039ff441a3d85c99a92
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-01 13:52:11 +0500

    Different statements in for loop

---
+++ Chapter 7/05- statements.py
@@ -0,0 +1,23 @@
+# Break statement
+
+for i in range (100):
+    if(i==51):
+        break
+    print(i)
+
+# Continue statements    
+
+for i in range (100):
+    if(i==51):
+        continue
+    print(i)
+
+# Pass statement
+
+for i in range(33):
+    pass
+
+i = 0
+while(i<45):
+    print(i)
+    i += 1
