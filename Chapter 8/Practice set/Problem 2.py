b38c58214c349cca60bc0b755ec58a6c46673bfa
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-01 19:34:55 +0500

    Converting celcius to fahrenheit

---
+++ Chapter 8/Practice set/Problem 2.py
@@ -0,0 +1,6 @@
+def Fahrenheit():
+    celcius = float(input("Enter the temperature : "))
+    fahrenheit = (celcius * 9/5) +32
+    print("Temperature in Fahrenheit : ", fahrenheit)
+
+Fahrenheit()
