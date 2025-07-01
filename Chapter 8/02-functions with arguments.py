286596021ce0b531138af960cf6739d7c6440460
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-01 18:02:42 +0500

    Arguments with functions

---
+++ Chapter 8/02-functions with arguments.py
@@ -0,0 +1,15 @@
+def goodDay(name, ending):
+    print("Good day, " + name)
+    print(ending)
+
+goodDay("Waqas",  "Thank You")
+goodDay("Arhan",  "Thank You")
+goodDay("Rehan",  "Thank You")
+
+
+def greet(name):
+    gr = "Hello, "  + name
+    return gr # using return value
+
+a = greet ("Harry")
+print(a)
