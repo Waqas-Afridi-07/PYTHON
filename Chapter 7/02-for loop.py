81982183dcb5eaca33989530f465b267d272ab40
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-01 13:03:09 +0500

    Learning about for loop

---
+++ Chapter 7/02-for loop.py
@@ -0,0 +1,25 @@
+fruits = ["Mango", "Watermelon", "Banana", "Apple"]
+
+for fruit in fruits:
+    print(fruit)
+
+# For loop with list
+
+l = [1,2,5,"Asim", True]
+
+for i in l:
+    print(i)
+
+# For loop with tuple
+
+t = (12.5, False, "Zia")
+
+for i in t:
+    print(i)
+
+# For loop with string
+
+s = "Waqas"
+
+for i in s:
+    print(i)
