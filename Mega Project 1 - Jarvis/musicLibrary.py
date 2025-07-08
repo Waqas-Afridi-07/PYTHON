78c6c29197bcc894513169934a99d9a92384d47d
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-08 19:35:11 +0500

    Jarvis project

---
+++ Mega Project 1 - Jarvis/musicLibrary.py
@@ -0,0 +1,7 @@
+# musicLibrary.py
+
+music = {
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    "faded": "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "shapeofyou": "https://www.youtube.com/watch?v=JGwWNGJdvx8"
+}
